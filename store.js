ffa8142f675d4b0a837921a863635f7d6ecefa5a
Swarnim Dhawane <Swarnim@gmail.com>
2024-04-24 00:25:36 +0530

    Updated Code 2.4

--- store.js
+++ store.js
@@ -3,6 +3,7 @@ const User = require('./models/User');
 const Event = require('./models/Event'); 
 const cors = require("cors"); 
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const admin = require('firebase-admin');
 
 const app = express()
@@ -16,14 +17,27 @@ connectDB();
 require('dotenv').config();
 
 
-// Allow requests from your Vercel-hosted frontend
-// app.use(cors({
-//     // origin: 'https://push-notify-frontend.vercel.app',
-//     origin: 'http://localhost:3001',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }));
+
 
 app.use(cors()); 
+
+const serviceAccount = {
+    "type": process.env.TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.UNIVERSE_DOMAIN
+  };
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 
 
@@ -43,50 +57,51 @@ app.post('/users', async (req, res) => {
 
 
 // Route to create a new event
+// Route to create a new event
 app.post('/events', async (req, res) => {
     try {
         const { date, description, userId } = req.body;
         const event = new Event({ date, description, userId });
         await event.save();
-
+ 
         console.log("Current user id is", userId);
-
+ 
         const otherUsers = await User.find({ uid: { $ne: userId } });
      
         // Project only email and pushNotificationToken fields
         const usersInfo = otherUsers.map(user => ({
             email: user.email,
-            pushNotificationToken: user.pushNotificationToken   
+            pushNotificationToken: user.pushNotificationToken  
         }));
-
+ 
         console.log("Other users in events are:", usersInfo);
-
+ 
         // Construct and send push notification messages to each user
         usersInfo.forEach(async (userInfo) => {
             const userToken = userInfo.pushNotificationToken;
             console.log("User token in store is", userToken);
-
+ 
             if (!userToken) {
                 console.error('Error: FCM token is missing for user:', userInfo.email);
                 return;
             }
-
+ 
             const message = {
-                data: {
+                notification: {
                     title: 'New Event Notification',
                     body: 'A new event has been added!'
                 },
                 token: userToken
             };
-
+ 
             try {
                 await admin.messaging().send(message);
-                console.log('Successfully sent message to user:', userInfo);
+                console.log('Successfully sent message to user:', userInfo.email);
             } catch (error) {
                 console.error('Error sending message to user:', userInfo.email, error);
             }
         });
-
+ 
         res.status(201).json({ message: 'Event added successfully', event });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -96,23 +111,7 @@ app.post('/events', async (req, res) => {
 
 
 
-const serviceAccount = {
-    "type": process.env.TYPE,
-    "project_id": process.env.PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.CLIENT_EMAIL,
-    "client_id": process.env.CLIENT_ID,
-    "auth_uri": process.env.AUTH_URI,
-    "token_uri": process.env.TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
-    "universe_domain": process.env.UNIVERSE_DOMAIN
-  };
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 
 
