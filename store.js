6b41ee0fad04cb7f39207a48974c6f7625b37345
Swarnim Dhawane <Swarnim@gmail.com>
2024-04-25 16:42:16 +0530

    Updated Code 4.3

--- store.js
+++ store.js
@@ -17,10 +17,10 @@ connectDB();
 require('dotenv').config();
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://push-notify-backend.vercel.app/',
+    origin: 'http://localhost:3001',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }));
-
 
 const serviceAccount = {
     "type": process.env.TYPE,
